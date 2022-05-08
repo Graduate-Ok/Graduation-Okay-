ab2295e7c9e145e667e39e6ee588c01b15139f24
subin Park <psb0320@hs.ac.kr>
2022-05-08 20:46:25 +0900

    220508 subin

---
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/pdfConfirm/DBConnection.java
@@ -0,0 +1,97 @@
+package com.graduate_ok.graduate_ok.pdfConfirm;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBConnection {
+    private static final String DB_DRIVER_CLASS = "org.mariadb.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mariadb://localhost:3306/graduate_ok";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "0320";
+
+    private static Connection getCon() {
+        Connection con = null;
+        try {
+            Class.forName(DB_DRIVER_CLASS);
+            con = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    // 기독교 과목 (from 교양 테이블)
+    public static List<String> getChristian() {
+        String sql = "SELECT ky_name1 FROM ky_course WHERE ky_name1 LIKE '%기독교%';";
+        return getListDataFromTable(sql);
+    }
+
+    // 성서 과목 (from 교양 테이블)
+    public static List<String> getBible() {
+        String sql = "SELECT ky_name1 FROM ky_course WHERE ky_name1 LIKE '%성서%';";
+        return getListDataFromTable(sql);
+    }
+
+    // 해당 학과 전공필수 과목 (from 전공, 전공필수 테이블)
+    public static List<String> getRequiredMajor(String major) {
+        String sql = "SELECT mrq_name FROM major_required_course r, major m"
+                + " WHERE r.major_no = m.major_no and m.major_name = '" + major + "';";
+        return getListDataFromTable(sql);
+    }
+
+    // 해당 학과 전공 최소이수학점 (from 전공 테이블)
+    public static int getMajorMinCredit(String major) {
+        String sql = "SELECT major_min_credit FROM major WHERE major_name = '" + major + "';";
+        return getIntDataFromTable(sql);
+    }
+
+
+    // DB에서 리스트 데이터 가져오기
+    private static List<String> getListDataFromTable(String sql) {
+        List<String> list = new ArrayList<>();
+
+        try {
+            Connection con = getCon();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+
+            while(rs.next()) {
+                list.add(rs.getString(1));
+            }
+
+            rs.close();
+            pstmt.close();
+            con.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    // DB에서 정수 데이터 가져오기
+    private static int getIntDataFromTable(String sql) {
+        int data = 0;
+
+        try {
+            Connection con = getCon();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                data = rs.getInt("major_min_credit");
+            }
+
+            rs.close();
+            pstmt.close();
+            con.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+}
--- graduate_ok/src/main/java/com/graduate_ok/graduate_ok/pdfConfirm/KyRequiredConfirm.java
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/pdfConfirm/KyRequiredConfirm.java
@@ -1,18 +1,8 @@
 package com.graduate_ok.graduate_ok.pdfConfirm;
 
-import java.io.File;
+import javax.persistence.criteria.CriteriaBuilder;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.*;
-
-import com.graduate_ok.graduate_ok.dto.KyCourseDto;
-import com.graduate_ok.graduate_ok.service.KyRequiredConfirmService;
-import lombok.RequiredArgsConstructor;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
 
 
 public class KyRequiredConfirm {
@@ -20,41 +10,52 @@ public class KyRequiredConfirm {
     private static String[] creditsCompleted = {"false", "false", "false"};
     // 채플/기독교/성서/대생길/사생길/독토,글쓰기/영어1, 영어2/진로와상담/비교과
     private static String[] isCompleted = {"false", "false", "false", "false", "false", "false", "false", "false"};
-    private static String[] Christian = {"기독교와문화", "기독교와인문학", "영화속의기독교", "기독교와세계종교"};
-    private static String[] bible = {"성서의세계", "성서와문학", "성서와여성", "성서와예술", "성서와평화", "성서와영성", "사건으로읽는성서"};
 
-    public static void main(String args[]) throws IOException {
+    // 기독교 과목 리스트 (from 교양 테이블)
+    private static List<String> christian = DBConnection.getChristian();
+    // 성서 과목 리스트 (from 교양 테이블)
+    private static List<String> bible= DBConnection.getBible();
 
+    public static void main(String args[]) throws IOException, NumberFormatException {
+        String[] list = PdfRead.PDFRead("C:/Users/수빈/Desktop/um72_0272003_r01.pdf");
 
+        for (String str : list) {
+            System.out.println(str);
+        }
 
-
-        String text = PDFRead("C:/Users/수빈/Desktop/um72_0272003_r01.pdf");
-
-        String[] list = text.split("\n");
-        int studentId = 0; //학번
+        // 학번
+        int studentId = 0;
+        // 비교과 이수 학기
+        int nonSubject = 0;
+        // 비교과 마일리지
         String mileage = "";
-        int extraCurr = 0;
+
         int gyoyang = 0;
         int major = 0;
-        // System.out.println(text);
         Map<String, Integer> SubEssential = new HashMap<>();
+
 
         for (int i = 0; i < list.length; i++) {
             String line = list[i];
+
             // 학번 추출
             if (line.contains("학 번")) {
                 studentId = Integer.parseInt(line.substring(4, 8));
             }
 
+            // 비교과 총 이수 학기 카운트
+            if (line.contains("학기 인정")) {
+                nonSubject++;
+            }
+
             // 마일리지 추출
             if (line.contains("마일리지")) {
                 mileage = line.substring(22, 25);
-                extraCurr = i - extraCurr - 1;
             }
 
-            // 비교과 이수 학기 확인
-            if (line.contains("비교과 프로그램")) {
-                extraCurr = i;
+            // 총 취득학점 추출
+            if (line.contains("총 취득학점")) {
+                //totalCredit = Integer.parseInt(line.substring(7, line.length() - 1));
             }
 
             // 전공, 교양 이수학점 확인
@@ -77,8 +78,7 @@ public class KyRequiredConfirm {
             }
         }
 
-        System.out.println("한신대 문서 맞는지? : " + VeificationPDF(text));
-        boolean check = checkEssential(SubEssential, studentId, Integer.parseInt(mileage), extraCurr);
+        boolean check = checkEssential(SubEssential, studentId, Integer.parseInt(mileage), nonSubject);
         boolean check2 = checkCredits(major, gyoyang, studentId);
         for (int i = 0; i < isCompleted.length; i++) System.out.println(isCompleted[i] + " ");
 
@@ -95,14 +95,12 @@ public class KyRequiredConfirm {
     }
 
     private static boolean checkEssential(Map<String, Integer> subject, int studentId, int mileage, int numExtra) {
-        // TODO Auto-generated method stub
-
         // 채플 이수
         if (subject.getOrDefault("채플", 0) >= 4) isCompleted[0] = "채플 : true";
         // 성서관련 과목중 1개이수 , 기독교관련 과목중 1개이수
         for (String key : subject.keySet()) {
-            if (Arrays.asList(Christian).contains(key) && subject.get(key) >= 1) isCompleted[1] = "기독교 : true";
-            if (Arrays.asList(bible).contains(key) && subject.get(key) >= 1) isCompleted[2] = "성서 : true";
+            if (christian.contains(key) && subject.get(key) >= 1) isCompleted[1] = "기독교 : true";
+            if (bible.contains(key) && subject.get(key) >= 1) isCompleted[2] = "성서 : true";
         }
 
         // 대생길 or 인증상담1 이수
@@ -138,31 +136,5 @@ public class KyRequiredConfirm {
         return false;
     }
 
-    // pdf 읽기
-    private static String PDFRead(String fileName) throws IOException {
-
-        File source = new File(fileName);
-        PDDocument pdfDoc = PDDocument.load(source);
-        String text = new PDFTextStripper().getText(pdfDoc);
-
-        return text;
-    }
-
-    // 문서내에 이런 단어들이 존재하는가?
-    // 한신대 성적 문서가 맞나?
-    private static boolean VeificationPDF(String text) {
-
-        String str1_verify = "* 이수구분변경은 포털>한신종합정보>성적>\"\r\n" +
-                "이수구분변경신청\"에서 신청하세요.\r\n";
-        String str2_verify = "*최초평점평균조회 : 포털>한신종합정보>성적\r\n" +
-                ">전체성적조회\r\n" + "";
-        String str3_verify = "* 졸업학점, 교양및전공필수 이수(미\r\n" + "이수)현황 :\r\n" +
-                "포털>한신종합정보>성적>\"교양 및 \r\n" + "전공필수 이수현황\"에서 확인하세요.\r\n" + "";
-
-        if (text.contains(str1_verify) && text.contains(str2_verify) && text.contains(str3_verify))
-            return true;
-
-        return false;
-    }
 
 }

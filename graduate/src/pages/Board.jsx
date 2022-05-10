08f8bb770c11117e6de430c6d379a0265424ebfa
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2022-05-10 14:03:55 +0900

    TEST
    지윤이 깃 테스트

--- graduate/src/pages/Board.jsx
+++ graduate/src/pages/Board.jsx
@@ -18,6 +18,7 @@ function Board(){
                             <div className = "Board__search">
                                 <select>
                                     <option value= "title">제목</option>
+                                    <option value = "content">내용</option>
                                     <option value = "writer">작성자</option>
                                 </select>
                                 <input type = "text" placeholder= "검색어를 입력하세요"></input>
@@ -35,21 +36,21 @@ function Board(){
                             <div className = "Board__content--number">3</div>
                             <div className = "Board__content--name">세번째 글입니다</div>
                             <div className = "Board__content--writer">배성규</div>
-                            <div className = "Board__content--date">12-31</div>
+                            <div className = "Board__content--date">2022-12-31</div>
                             <div className = "Board__content--hits">18</div>
                         </div>
                         <div className = "Board__content--content">
                             <div className = "Board__content--number">2</div>
                             <div className = "Board__content--name">두번째 글입니다</div>
                             <div className = "Board__content--writer">배성규</div>
-                            <div className = "Board__content--date">12-31</div>
+                            <div className = "Board__content--date">2022-12-31</div>
                             <div className = "Board__content--hits">1</div>
                         </div>
                         <div className = "Board__content--content">
                             <div className = "Board__content--number">1</div>
                             <div className = "Board__content--name">첫번째 글입니다</div>
                             <div className = "Board__content--writer">배성규</div>
-                            <div className = "Board__content--date">12-31</div>
+                            <div className = "Board__content--date">2022-12-31</div>
                             <div className = "Board__content--hits">7</div>
                         </div>
                         <div className= "Board__footer">

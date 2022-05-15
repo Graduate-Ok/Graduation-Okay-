82745dbb2664e8c838f9d6a7aab92bf8420e6a6a
JiYoon0712 <tkstpqpfldk9@naver.com>
2022-05-15 18:35:02 +0900

    2022.05.15
    - 졸업요건확인 페이지 수정

--- graduate/src/pages/GraduateOk.jsx
+++ graduate/src/pages/GraduateOk.jsx
@@ -10,10 +10,30 @@ function GraduateOk() {
                     <div className='Graduate__check'>
 
                         <div className='Graduate_Possible'>
-                            졸업 가능 <br/><br/>
+                            <br />졸업 가능 <br /><br />
                         </div>
 
-                        <div className='Lack__Graduate__Credit'>
+                        <div>
+                        <table className='OkTable'>
+                            <tr className='Ok__menu'>
+                                <th className='Lack__Graduate__Credit'> 졸업 학점</th>
+                                <th className='Lack__MajorCredit'>전공 학점 </th>
+                                <th className='Lack__KyCredit'> 교양 학점 </th>
+                                <th className='Lack__NonSub'> 비교과 이수 여부 </th>
+                            </tr>
+                            
+                            <tr>
+                                <td className='Lack__Graduate__Credit'> 130 </td>
+                                <td className='Lack__MajorCredit'> 80 </td>
+                                <td className='Lack__KyCredit'> 50 </td>
+                                <td className='Lack__NonSub'> pass </td>
+                            </tr>
+
+
+                        </table>
+                        </div>
+
+                        {/* <div className='Lack__Graduate__Credit'>
                             졸업 학점 20 미달
                         </div>
                         <div className='Lack__MajorCredit'>
@@ -21,7 +41,7 @@ function GraduateOk() {
                         </div>
                         <div className='Lack__KyCredit'>
                             교양 학점 20 미달
-                        </div>
+                        </div> */}
 
                     </div>
                 </section>

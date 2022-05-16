fde8c76b688b4fcc873d8c828a17c8d27253a28a
subin Park <psb0320@hs.ac.kr>
2022-05-16 20:42:36 +0900

    feat: SWAGGER UI 연동
    - Swagger UI 라이브러리 연동
    - 관련 API 정보 추가

--- graduate_ok/src/main/java/com/graduate_ok/graduate_ok/controller/PdfRestController.java
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/controller/PdfRestController.java
@@ -1,31 +1,60 @@
 package com.graduate_ok.graduate_ok.controller;
 
 import com.graduate_ok.graduate_ok.pdfCheck.PdfCheck;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.apache.tika.Tika;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import java.io.File;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Objects;
 
 @RestController
 public class PdfRestController {
 
     @PostMapping("/fileUpload")
-    public Integer upload(MultipartHttpServletRequest mRequest) {
+    @ApiOperation(value = "PDF 관련 로직 API", notes = "관련 PDF 검사 관련 로직")
+    @ApiParam("관련 PDF (중요)")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "ok"),
+            @ApiResponse(code = 500, message = "server error"),
+            @ApiResponse(code = 404, message = "not found")
+    })
+    public HashMap<String, Object> upload(MultipartHttpServletRequest mRequest) {
 
         try {
             MultipartFile file = mRequest.getFile("file");
-
             assert file != null;
-            File convFile = new File(System.getProperty("java.io.tmpdir")+"/"+file.getOriginalFilename());
+            InputStream is = file.getInputStream();
+            String mimeType = new Tika().detect(is);
+
+            if (!Objects.requireNonNull(file.getOriginalFilename()).toLowerCase(Locale.ROOT).endsWith(".pdf") || !isPdfMimeType(mimeType)) {
+                return new HashMap<>();
+            }
+
+            File convFile = new File(System.getProperty("java.io.tmpdir") + "/" + file.getOriginalFilename());
             file.transferTo(convFile);
 
             return PdfCheck.execute(convFile.getAbsolutePath());
 
         } catch (Exception e) {
             e.printStackTrace();
-            return 0;
+            return new HashMap<>();
         }
+    }
+
+    private boolean isPdfMimeType(String mimeType) {
+        if (mimeType == null || mimeType.equals("")) {
+            return false;
+        }
+        return mimeType.equals("application/pdf");
     }
 }

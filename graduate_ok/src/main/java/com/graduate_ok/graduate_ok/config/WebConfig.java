208f6679d240e41153ec9caeae33e0b2720db31f
subin Park <psb0320@hs.ac.kr>
2022-05-23 20:59:35 +0900

    feat: Server & Client 구조 연동 설정
    - CORS 설정을 통한 프로젝트 연동
    - 테스트 완료

---
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.graduate_ok.graduate_ok.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(3000);
+    }
+
+}
--- graduate_ok/src/main/java/com/graduate_ok/graduate_ok/controller/NoticeRestController.java
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/controller/NoticeRestController.java
@@ -1,10 +1,7 @@
 package com.graduate_ok.graduate_ok.controller;
 
 import com.graduate_ok.graduate_ok.dto.NoticeListDto;
-import com.graduate_ok.graduate_ok.service.NoticeService;
 import com.graduate_ok.graduate_ok.service.serviceImpl.NoticeServiceImpl;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;

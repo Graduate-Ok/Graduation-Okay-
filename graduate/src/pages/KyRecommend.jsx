702e764d0fcaa2597bc997a73214e17386b4cc1e
JiYoon0712 <tkstpqpfldk9@naver.com>
2022-05-24 20:36:32 +0900

    2022.05.24
    - KyRecommend.css : 글자 가운데 정렬
    - KyRecommend.jsx : 불필요한 정보 삭제

--- graduate/src/pages/KyRecommend.jsx
+++ graduate/src/pages/KyRecommend.jsx
@@ -42,41 +42,7 @@ function KyRecommend() {
                                         return <KyRecommendRow KyRecommend={e} />
                                     })
                                 }
-                            {/* <tr>
-                                <td className="grade_table_td rank_width">1</td>
-                                <td className="grade_table_td name_width">오르간연주법1</td>
-                                <td className="grade_table_td ky_width">소통하는 지성인</td>
-                                <td className="grade_table_td core_width">인문</td>
-                                <td className="grade_table_th gradenum_width" >2</td>
-                                <td className="grade_table_td num_width">1000</td>
-                            </tr>
-
-                            <tr>
-                                <td className="grade_table_td rank_width">2</td>
-                                <td className="grade_table_td name_width">감정수업:침묵과말</td>
-                                <td className="grade_table_td ky_width">도전하는 창의인</td>
-                                <td className="grade_table_td core_width">창의융합</td>
-                                <td className="grade_table_th gradenum_width" >3</td>
-                                <td className="grade_table_td num_width">1000</td>
-                            </tr>
-
-                            <tr>
-                                <td className="grade_table_td rank_width">3</td>
-                                <td className="grade_table_td name_width">기초영어회화1 </td>
-                                <td className="grade_table_td ky_width">실천하는 평화인</td>
-                                <td className="grade_table_td core_width">글로벌</td>
-                                <td className="grade_table_th gradenum_width" >3</td>
-                                <td className="grade_table_td num_width">900</td>
-                            </tr>
-
-                            <tr>
-                                <td className="grade_table_td rank_width">4</td>
-                                <td className="grade_table_td name_width">물리란무엇인가</td>
-                                <td className="grade_table_td ky_width">도전하는 창의인</td>
-                                <td className="grade_table_td core_width">지식정보</td>
-                                <td className="grade_table_th gradenum_width" >3</td>
-                                <td className="grade_table_td num_width">800</td>
-                            </tr>*/}
+                            
 
                         </table>
                     </div>

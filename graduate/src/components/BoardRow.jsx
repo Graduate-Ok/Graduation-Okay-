6b31b552a6ccd198849108f61bc16425157b5bb7
JiYoon0712 <tkstpqpfldk9@naver.com>
2022-05-24 19:30:56 +0900

    2022.05.24
    -ViewBoard

--- graduate/src/components/BoardRow.jsx
+++ graduate/src/components/BoardRow.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const BoardRow = ({Board}) => {
     console.log(Board.brdKey);
     return(
         <div className="Board__content--content">
             <div className="Board__content--number">{Board.brdKey}</div>
-            <div className="Board__content--name">{Board.brdTitle}</div>
+            <Link to="ViewBoard" className="Board__content--name">{Board.brdTitle}</Link>
             <div className="Board__content--writer">{Board.brdWriter}</div>
             <div className="Board__content--date">{Board.brdWtTime.substring(0, 10) + ' ' + Board.brdWtTime.substring(11,16)}</div>
             <div className="Board__content--hits">{Board.brdLookup}</div>
         </div>
-    )
+    )   
 }
 
 export default BoardRow;

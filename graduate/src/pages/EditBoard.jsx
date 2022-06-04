429108bf92ebe69622d13302a8e12b069782fbc0
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2022-06-04 22:40:41 +0900

    Create EditBoard.jsx

---
+++ graduate/src/pages/EditBoard.jsx
@@ -0,0 +1,148 @@
+import { Link, Navigate, useNavigate, useParams } from 'react-router-dom';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import useInput from '../hooks/useInput';
+
+// 글 수정 페이지
+// localhost:3000/Board/EditBoard
+
+const EditBoard = ({ brdKey }) => {
+    const navigate = useNavigate(); // form 제출 후 /Board로 돌아가는 코드
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post('http://localhost:8089/Board/PostBoard', {
+            brdTitle: title,
+            brdWriter: writer,
+            brdContent: content,
+            brdPassword: password,
+        });
+        navigate('/Board');
+    };
+
+    const [title, onChangeTitle] = useInput('');
+    const [writer, onChangeWriter] = useInput('');
+    const [password, onChangePassword] = useInput('');
+    const [content, onChangeContent] = useInput('');
+
+    const [inputData, setInputData] = useState([]);
+
+    let params = useParams().brdKey;
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get(
+                `http://localhost:8089/Board/${params}`,
+            );
+            setInputData(response.data);
+        };
+        fetchData();
+    }, []);
+    console.log(brdKey);
+    return (
+        <>
+            <main>
+                <div className="Board">
+                    <div className="BoardSize">
+                        <div className="Board__header">
+                            <p className="minititle"> 정보공유 게시판</p>
+                        </div>
+
+                        <form
+                            name="writing"
+                            method="post"
+                            action="/Board/EditBoard"
+                        >
+                            <div className="Board__writecontainer">
+                                <label
+                                    for="edit"
+                                    className="Board__writecontainer--head"
+                                >
+                                    {' '}
+                                    [ 정보 공유 게시판 ]{' '}
+                                </label>
+
+                                <div className="Board__writecontainer--info">
+                                    제목{' '}
+                                    <div className="Board__writecontainer--detail">
+                                        {''}
+                                        <input
+                                            type="text"
+                                            onChange={onChangeTitle}
+                                            placeholder={inputData.brdTitle}
+                                            name="brdTitle"
+                                            id="brdTitle"
+                                            className="Board__writecontainer--title"
+                                            autoFocus
+                                            required
+                                        ></input>
+                                    </div>
+                                    <br />
+                                    작성자{' '}
+                                    <div className="Board__writecontainer--detail">
+                                        {' '}
+                                        <input
+                                            type="text"
+                                            onChange={onChangeWriter}
+                                            placeholder="작성자의 이름을 입력하세요."
+                                            name="brdWriter"
+                                            id="brdWriter"
+                                            className="Board__writecontainer--writer"
+                                            required
+                                        ></input>
+                                    </div>
+                                    <br /> Password (수정/삭제시 비밀번호가
+                                    필요합니다.)
+                                    <div className="Board__writecontainer--detail">
+                                        {' '}
+                                        <input
+                                            type="password"
+                                            onChange={onChangePassword}
+                                            placeholder="비밀번호를 입력하세요."
+                                            name="brdPassword"
+                                            id="brdPassword"
+                                            className="Board__writecontainer--password"
+                                            required
+                                        ></input>
+                                    </div>
+                                    <br />
+                                    내용{' '}
+                                    <div className="Board__writecontainer--detail">
+                                        <textarea
+                                            placeholder="내용을 입력하세요."
+                                            onChange={onChangeContent}
+                                            name="brdContent"
+                                            id="brdContent"
+                                            className="Board__writecontainer--content"
+                                            required
+                                        ></textarea>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="Board__footer">
+                                <div
+                                    onClick={() => navigate(-1)}
+                                    className="Board__footer--button"
+                                >
+                                    돌아가기
+                                </div>
+                                <button
+                                    onClick={handleSubmit}
+                                    type="submit"
+                                    id="submit"
+                                    name="submit"
+                                    className="Board__footer--button"
+                                >
+                                    등록하기
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
+};
+
+export default EditBoard;

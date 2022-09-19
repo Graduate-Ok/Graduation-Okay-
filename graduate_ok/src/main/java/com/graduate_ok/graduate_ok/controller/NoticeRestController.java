23930e661ce6c84e95fff3af191bdf5f3aa3ebb7
pangkyu <baesense@naver.com>
2022-09-20 00:30:43 +0900

    2

--- graduate_ok/src/main/java/com/graduate_ok/graduate_ok/controller/NoticeRestController.java
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/controller/NoticeRestController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/Notice")
 @RequiredArgsConstructor
 public class NoticeRestController {
--- graduate_ok/src/main/java/com/graduate_ok/graduate_ok/controller/PdfRestController.java
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/controller/PdfRestController.java
@@ -19,6 +19,7 @@ import java.util.Locale;
 import java.util.Objects;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class PdfRestController {
 
     @PostMapping("/Graduate")

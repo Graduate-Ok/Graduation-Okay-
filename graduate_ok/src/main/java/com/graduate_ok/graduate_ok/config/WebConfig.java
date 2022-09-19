5567949ac44840fdbcc51cf73e318a77a96c2ad1
pangkyu <baesense@naver.com>
2022-09-20 00:58:34 +0900

    Update WebConfig.java

--- graduate_ok/src/main/java/com/graduate_ok/graduate_ok/config/WebConfig.java
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/config/WebConfig.java
@@ -2,16 +2,17 @@ package com.graduate_ok.graduate_ok.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://13.125.25.62")
                 .allowedMethods("GET", "POST", "PUT", "DELETE" , "OPTIONS")
 //                .allowedMethods("*")
                 .allowedHeaders("Origin","Accept","X-Requested-With","Content-Type","Access-Control-Request-Method","Access-Control-Request-Headers","Authorization")

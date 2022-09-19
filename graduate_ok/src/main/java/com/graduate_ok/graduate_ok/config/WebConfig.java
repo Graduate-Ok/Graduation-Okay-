80cf70a2447f61177fcd8c4b79ad12ec6fcacbcd
subin Park <psb0320@hs.ac.kr>
2022-09-19 21:52:59 +0900

    feat: Webconfig allowedOrigins 추가링크 삭제

--- graduate_ok/src/main/java/com/graduate_ok/graduate_ok/config/WebConfig.java
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/config/WebConfig.java
@@ -11,14 +11,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://13.125.25.62:3000", "http://localhost:8089", "http://13.125.25.62:8089", "http://13.125.25.62:3000/KyRecommend/")
+                .allowedOrigins("http://localhost:3000", "http://13.125.25.62:3000", "http://localhost:8089", "http://13.125.25.62:8089")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
 //                .allowedMethods("*")
-                .maxAge(3000)
-//                .allowedHeaders("*")
                 .allowedHeaders("Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization")
-                .exposedHeaders("*");
-//                .allowCredentials(true);
+//                .allowedHeaders("*")
+                .exposedHeaders("*")
+//                .allowCredentials(true)
+                .maxAge(3000);
     }
 
 }

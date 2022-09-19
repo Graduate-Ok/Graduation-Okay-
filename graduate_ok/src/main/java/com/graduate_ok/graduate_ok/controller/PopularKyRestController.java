c6766169a0d81224b4a6f390284ba80d167c0ac4
pangkyu <baesense@naver.com>
2022-09-20 01:13:55 +0900

    solve

--- graduate_ok/src/main/java/com/graduate_ok/graduate_ok/controller/PopularKyRestController.java
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/controller/PopularKyRestController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/KyRecommend")
 @RequiredArgsConstructor
 public class PopularKyRestController {

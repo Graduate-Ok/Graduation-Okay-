f2d82d1c292acbd0b58cbd52b58a21f7cf75090b
subin Park <psb0320@hs.ac.kr>
2022-09-19 21:13:53 +0900

    feat: Webconfig 수정

--- graduate_ok/src/main/java/com/graduate_ok/graduate_ok/config/WebConfig.java
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/config/WebConfig.java
@@ -1,23 +1,24 @@
-//package com.graduate_ok.graduate_ok.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//        //        .allowedOrigins("http://localhost:3000", "http://13.125.25.62:3000")
-//
-////                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+package com.graduate_ok.graduate_ok.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://13.125.25.62:3000", "http://localhost:8089", "http://13.125.25.62:8089")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
 //                .allowedMethods("*")
-//                .maxAge(3000)
-//                .allowedHeaders("*");
-//    }
-//
-//}
+                .maxAge(3000)
+//                .allowedHeaders("*")
+                .allowedHeaders("Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization")
+                .exposedHeaders("*");
+//                .allowCredentials(true);
+    }
+
+}
--- graduate_ok/src/main/java/com/graduate_ok/graduate_ok/controller/BoardRestController.java
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/controller/BoardRestController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RequestMapping("/Board")
 @ApiResponses({

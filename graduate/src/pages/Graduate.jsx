371ccf83fe3fd0b36bb703d753c216f46a875aa0
JiYoon0712 <tkstpqpfldk9@naver.com>
2022-09-21 00:15:02 +0900

    정보요

--- graduate/src/pages/Graduate.jsx
+++ graduate/src/pages/Graduate.jsx
@@ -65,6 +65,9 @@ const Graduate = () => {
                     <div className="section__text">
                         당신은 졸업이 가능한가요 ?
                     </div>
+                    <div className="section__text">
+                        교양과목 정보 외의 인적사항 및 학점은 따로 저장하지 않습니다!
+                    </div>
                     <form method="post" action="/Graduate">
                         <div className="section__button">
                             <label className="upload__button" for="file">

4c624e3f52bd64b49fe3ea7875828b59feb7ed44
subin Park <psb0320@hs.ac.kr>
2022-09-26 03:22:02 +0900

    feat: (서버) 작성시간 관련 변경 - 테스트 후 불필요한 Timestamp 관련 클래스와 코드 삭제

--- graduate_ok/src/main/java/com/graduate_ok/graduate_ok/service/serviceImpl/NoticeServiceImpl.java
+++ graduate_ok/src/main/java/com/graduate_ok/graduate_ok/service/serviceImpl/NoticeServiceImpl.java
@@ -3,13 +3,11 @@ package com.graduate_ok.graduate_ok.service.serviceImpl;
 import com.graduate_ok.graduate_ok.dto.NoticeDto;
 import com.graduate_ok.graduate_ok.dto.NoticeListDto;
 import com.graduate_ok.graduate_ok.dto.SearchHelper;
-import com.graduate_ok.graduate_ok.dto.TimestampConverter;
 import com.graduate_ok.graduate_ok.mapper.NoticeMapper;
 import com.graduate_ok.graduate_ok.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
 import java.util.List;
 
 @Service
@@ -32,25 +30,6 @@ public class NoticeServiceImpl implements NoticeService {
         searchHelper = new SearchHelper(totalCount, page, notiCategory, srchType, srchKeyword);
 
         List<NoticeDto> list = noticeMapper.selectNoticeList(searchHelper);
-
-        // 타임스탬프 시간 변경 (9시간 추가) // 로컬에서 돌릴 때 해당
-//        for (NoticeDto nd : list) {
-//            Timestamp original = nd.getNotiWtTime();
-//            Calendar cal = Calendar.getInstance();
-//            cal.setTimeInMillis(original.getTime());
-//            cal.add(Calendar.HOUR, 9);
-//            Timestamp change = new Timestamp(cal.getTime().getTime());
-//            nd.setNotiWtTime(change);
-//        }
-
-        // Unix Time -> Timestamp 변환
-//        for (NoticeDto bd : list) {
-//            // Timestamp
-//            Timestamp timestamp = TimestampConverter.timestampConverter(bd.getNotiWtTime());
-//            // String
-////            Timestamp timestamp = TimestampConverter.timestampConverter(bd.getNotiWtTime().toString());
-//            bd.setNotiWtTime(timestamp);
-//        }
 
         noticeListDto.setNoticeDtoList(list);
         noticeListDto.setSearchHelper(searchHelper);

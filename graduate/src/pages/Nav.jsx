d2c0225a81370945f381989ed028d229f053c5b7
pangkyu <baesense@naver.com>
2022-10-02 19:28:22 +0900

    design : nav 위치 변경

--- graduate/src/pages/Nav.jsx
+++ graduate/src/pages/Nav.jsx
@@ -13,11 +13,11 @@ function Nav() {
                 <Link to="Notice" className="navbar__nav">
                     공지사항
                 </Link>
-                <Link to="Graduate" className="navbar__nav">
-                    졸업요건확인
-                </Link>
                 <Link to="KyRecommend" className="navbar__nav">
                     인기교양추천
+                </Link>
+                <Link to="Graduate" className="navbar__nav">
+                    졸업요건확인
                 </Link>
                 <Link to="Board" className="navbar__nav">
                     정보공유게시판

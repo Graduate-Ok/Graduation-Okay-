530e51536c091f461c7dac7b43ffa3e313a176ca
JiYoon0712 <tkstpqpfldk9@naver.com>
2022-10-06 21:03:53 +0900

    게시글 줄바꿈 해결
    - 정보 공유 게시판
    > 띄어쓰기 적용
    > 행간 조절
    
    - 인기 교양 추천
    > 내용 수정
    > 에타 버튼 누르면 연결
    > 수강횟수순 버튼 hover 삭제

--- graduate/src/components/ViewBoardRow.jsx
+++ graduate/src/components/ViewBoardRow.jsx
@@ -27,7 +27,7 @@ const ViewBoardRow = ({ ViewBoard }) => {
             </div>
 
             <div className="BRD_title"> {ViewBoard.brdTitle} </div>
-            <div className="BRD_content"> {ViewBoard.brdContent} </div>
+            <div className="BRD_content"> <pre>{ViewBoard.brdContent}</pre> </div>
         </div>
     );
 };

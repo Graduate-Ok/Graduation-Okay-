60127a954dc83cd6916333517d956d6633bb3533
JiYoon0712 <tkstpqpfldk9@naver.com>
2022-10-09 22:24:04 +0900

    정보공유게시판 글 튀어나옴

--- graduate/src/components/ViewBoardRow.jsx
+++ graduate/src/components/ViewBoardRow.jsx
@@ -27,7 +27,7 @@ const ViewBoardRow = ({ ViewBoard }) => {
             </div>
 
             <div className="BRD_title"> {ViewBoard.brdTitle} </div>
-            <div className="BRD_content"> <pre>
+            <div className="BRD_content"> <pre className="BRD_content--detail">
           {ViewBoard.brdContent}</pre> </div>
         </div>
     );

173a5c64c1ff386f023141318e70fbc081e90873
pangkyu <baesense@naver.com>
2023-02-08 16:14:50 +0900

    Feat : 네비게이션 바 반응형적용
    모바일 뷰 환경일시 네비게이션 바 토글모드로 변경

--- graduate/src/components/Header.jsx
+++ graduate/src/components/Header.jsx
@@ -1,9 +1,16 @@
 import '../css/Header.css';
+import Sidebar from './Sidebar';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 /**
  * @description Header 컴포넌트
  */
 function Header() {
+    const [isToggled, setIsToggled] = useState(false);
+    const handleToggleSide = () => {
+        setIsToggled(!isToggled);
+    };
     return (
         <>
             <header className="header">
@@ -14,6 +21,45 @@ function Header() {
                         alt="headerimg"
                     ></img>
                 </a>
+
+                {isToggled ? (
+                    <div className="header__sidebar">
+                        <img
+                            className="header__sidebar-icon"
+                            src="imgs/sidebar.png"
+                            alt="icon"
+                            onClick={handleToggleSide}
+                        ></img>
+                    </div>
+                ) : (
+                    <div className="header__sidebar--toggleMode">
+                        <div>
+                            <div>
+                                <img
+                                    alt="reject"
+                                    src="imgs/reject.png"
+                                    className="header__sidebar-reject"
+                                    onClick={handleToggleSide}
+                                ></img>
+                            </div>
+                            <Link to="Notice" className="navbar__nav">
+                                공지사항
+                            </Link>
+                            <Link to="KyRecommend" className="navbar__nav">
+                                인기교양추천
+                            </Link>
+                            <Link to="Graduate" className="navbar__nav">
+                                졸업요건확인
+                            </Link>
+                            <Link to="Board" className="navbar__nav">
+                                정보공유게시판
+                            </Link>
+                            <Link to="Feedback" className="navbar__nav">
+                                피드백하기
+                            </Link>
+                        </div>
+                    </div>
+                )}
             </header>
         </>
     );
